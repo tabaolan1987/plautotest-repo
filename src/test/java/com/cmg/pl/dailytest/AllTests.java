098cdf9b30162761c4c3fb3cae048329463e79ac
manh.trinh <manh.trinh@c-mg.com>
2013-12-31 09:45:21 +0700

    test case mapping

--- src/test/java/com/cmg/pl/dailytest/AllTests.java
+++ src/test/java/com/cmg/pl/dailytest/AllTests.java
@@ -7,14 +7,14 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ 
 		PLDailyTestCase1Test.class,
-		PLDailyTestCase2Test.class,
-		PLDailyTestCase3Test.class,
-		PLDailyTestCase4Test.class,
-		PLDailyTestCase5Test.class,
-		PLDailyTestCase6Test.class,
-		PLDailyTestCase7Test.class,
-		PLDailyTestCase8Test.class,
-		PLDailyTestCase9Test.class,
+//		PLDailyTestCase2Test.class,
+//		PLDailyTestCase3Test.class,
+//		PLDailyTestCase4Test.class,
+//		PLDailyTestCase5Test.class,
+//		PLDailyTestCase6Test.class,
+//		PLDailyTestCase7Test.class,
+//		PLDailyTestCase8Test.class,
+//		PLDailyTestCase9Test.class,
 		PLDailyTestCase10Test.class 
 		})
 public class AllTests {
