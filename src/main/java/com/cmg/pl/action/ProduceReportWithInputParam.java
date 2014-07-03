cecda5e5b8a50e024af4c796fe725bf54fde2a61
lan.ta <lan.ta@c-mg.com>
2014-07-03 08:49:05 +0700

    update

--- src/main/java/com/cmg/pl/action/ProduceReportWithInputParam.java
+++ src/main/java/com/cmg/pl/action/ProduceReportWithInputParam.java
@@ -1,12 +1,9 @@
 package com.cmg.pl.action;
 
 
-import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
