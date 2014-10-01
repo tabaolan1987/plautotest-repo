9f0a605f8ce2c96f57e3d337169f8882a5a36854
lan.ta <lan.ta@c-mg.com>
2014-10-01 10:26:55 +0700

    update

--- src/main/java/com/cmg/pl/listenner/ItestListen.java
+++ src/main/java/com/cmg/pl/listenner/ItestListen.java
@@ -5,21 +5,38 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.c_mg.pl.selenium.PLAUTOTEST.DriverUtil;
 import com.c_mg.pl.selenium.PLAUTOTEST.TakeScreenShot;
 
 
 public class ItestListen implements ITestListener {
-
-
+	
+	//private static ScreenRecord record;
+	
 	public void onTestSuccess(ITestResult result) {
+		/*try {
+			record.stopRecording();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
 	}
 
 	public void onTestFailure(ITestResult result) {
 		TakeScreenShot.takeSnapShot(result.getName());
+		/*try {
+			record.stopRecording();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
 	}
 
 	public void onTestSkipped(ITestResult result) {
 		TakeScreenShot.takeSnapShot(result.getName());
+		/*try {
+			record.stopRecording();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
 	}
 
 	public void onFinish(ITestContext context) {
@@ -27,7 +44,13 @@ public class ItestListen implements ITestListener {
 	}
 
 	public void onTestStart(ITestResult result) {
-
+		String name = DriverUtil.browserRunning+"-"+ result.getName();
+		/*try {
+			record = new ScreenRecord();
+			record.startRecording(name);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
