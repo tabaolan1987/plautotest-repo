805ad558bfd57e5ec54c9a10efd784dbf2904f0a
lan.ta <lan.ta@c-mg.com>
2014-10-07 08:40:31 +0700

    update

--- src/test/java/com/cmg/pl/dailytest/Cucumber/Define/LoginDefine.java
+++ src/test/java/com/cmg/pl/dailytest/Cucumber/Define/LoginDefine.java
@@ -1,18 +1,18 @@
 package com.cmg.pl.dailytest.Cucumber.Define;
 
-
 import com.c_mg.pl.selenium.PLAUTOTEST.DriverUtil;
 import com.cmg.pl.action.Authenticate;
 import com.cmg.pl.action.SuperUser;
+
 import cucumber.api.java.en.Then;
 
-public class DefineLoadMember01 {
+public class LoginDefine {
 	
 	@Then("^I login as superuser$")
 	public void i_login_as_superuser() throws Throwable {
 		Authenticate.Login(DriverUtil.driverCurrent, "Autosuperuser", "P3nsions");
 	}
-
+	
 	@Then("^I load member \"(.*?)\"$")
 	public void i_load_member(String arg1) throws Throwable {
 		String group = arg1.split("-")[0];
@@ -20,9 +20,4 @@ public class DefineLoadMember01 {
 		SuperUser.loadMember(DriverUtil.driverCurrent, 30, group, refno);
 	}
 
-	
-	
-	
-	
-	
 }
