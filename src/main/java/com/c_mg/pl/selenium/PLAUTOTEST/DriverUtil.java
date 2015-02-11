74a764b2bbdd3bbc0c91bf7ead334a96fd8e5fbf
lan.ta <lan.ta@c-mg.com>
2015-02-11 02:40:24 +0000

    update

--- src/main/java/com/c_mg/pl/selenium/PLAUTOTEST/DriverUtil.java
+++ src/main/java/com/c_mg/pl/selenium/PLAUTOTEST/DriverUtil.java
@@ -24,9 +24,9 @@ public class DriverUtil {
 	public static WebDriver driverCurrent;
 	
 	public static String getIeDriver(){
-		/*String path = "H:\\Driver Automation\\IEDriverServer.exe";
-		return path;*/
-		String path = PropertiesHelper.getKey(PROP_PROJECT_BASE_DIR) + File.separator + FOLDER_DRIVER 
+		String path = "H:\\Driver Automation\\IEDriverServer.exe";
+		return path;
+		/*String path = PropertiesHelper.getKey(PROP_PROJECT_BASE_DIR) + File.separator + FOLDER_DRIVER 
 				+ File.separator + "IEDriverServer.exe";
 		try {
 			File driverIe = new File(path);
@@ -37,13 +37,13 @@ public class DriverUtil {
 			e.printStackTrace();
 			return null;
 		}
-		return null;
+		return null;*/
 	}
 	
 	public static String getChromeDriver(){
-		/*String path = "H:\\Driver Automation\\chromedriver.exe";
-		return path;*/
-		String path = PropertiesHelper.getKey(PROP_PROJECT_BASE_DIR) + File.separator + FOLDER_DRIVER 
+		String path = "H:\\Driver Automation\\chromedriver.exe";
+		return path;
+		/*String path = PropertiesHelper.getKey(PROP_PROJECT_BASE_DIR) + File.separator + FOLDER_DRIVER 
 				+ File.separator + "chromedriver.exe";
 		try {
 			File driverChrome = new File(path);
@@ -54,7 +54,7 @@ public class DriverUtil {
 			e.printStackTrace();
 			return null;
 		}
-		return null;	
+		return null;	*/
 	}
 	
 	public static WebDriver getInstance(String browser){
